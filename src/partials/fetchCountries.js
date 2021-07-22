556518aa04c220a1b98209e1b9401efa997b0f28
Kostia Chaikovskyi <chaichka175@gmail.com>
2021-07-22 11:27:58 +0300

    async/await add

--- src/partials/fetchCountries.js
+++ src/partials/fetchCountries.js
@@ -1,11 +1,22 @@
-export default function fetchCountries(countryName) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${countryName}?fields=name;capital;population;flag;languages`).then(r => {
-        if (!r.ok) {
-            throw new Error(r.status);
-      }
-      return r.json()
-  })
+// export default function  fetchCountries(countryName) {
+//   return fetch(`https://restcountries.eu/rest/v2/name/${countryName}?fields=name;capital;population;flag;languages`).then(r => {
+//         if (!r.ok) {
+//             throw new Error(r.status);
+//       }
+//       return r.json()
+//   })
+// }
+
+export default async function fetchCountries(countryName) {
+  const response = await fetch(`https://restcountries.eu/rest/v2/name/${countryName}?fields=name;capital;population;flag;languages`);
+  if (!response.ok) {
+    throw new Error(response.status)
+  }
+  const countries = await response.json();
+  return countries;
+  
 }
+
 
 
 
